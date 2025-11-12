9c388f3667dd956f1a9fa8b77aa73244f382835c
Joseph TAMO <tingangt@gmail.com>
2025-11-11 21:55:12 -0500

    :sparkles: Update yaml & model

--- src/main/java/dev/tamal/example/demovalidationspring/model/Customer.java
+++ src/main/java/dev/tamal/example/demovalidationspring/model/Customer.java
@@ -1,6 +1,5 @@
 package dev.tamal.example.demovalidationspring.model;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
@@ -10,7 +9,7 @@ public record Customer(
         @NotBlank String firstName,
         String lastName,
         @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$") String email,
-        @NotBlank @Min(6) String address,
+        @NotBlank String address,
         LocalDate birthDate
 ) {
 }
